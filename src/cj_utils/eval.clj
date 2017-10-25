d3af21c042db39195476ba92a71b2a76838a12d2
Juhezi <juhezix@163.com>
2017-10-25 22:38:32 +0800

    [Add] eval

---
+++ src/cj_utils/eval.clj
@@ -0,0 +1,14 @@
+(ns cj-utils.eval)
+
+(defn eval [exp env]
+  )
+
+(defn self-evaluating? [exp]
+  "自求值检测"
+  false)
+
+
+
+(defn assignment? [exp]
+  "赋值检测"
+  false)
--- test/cj_utils/temp/temp_test.clj
+++
@@ -1,6 +0,0 @@
-(ns cj-utils.temp.temp-test
-  (:require [clojure.test :refer :all]))
-
-(deftest a-test
-  (testing "Failed"
-    (is (=  1))))
